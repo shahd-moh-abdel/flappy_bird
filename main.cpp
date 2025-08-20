ab58d88f6153c2d16934eca3d0685eec72f2db1f
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-08-20 16:43:53 +0300

    added bird

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <ostream>
 #include <raylib.h>
 #include <raymath.h>
 #include "colors.h"
 #define SCREEN_WIDTH 450
 #define SCREEN_HEIGHT 800
 
+class Bird {
+public:
+  Vector2 pos;
+  int width;
+  int height;
+  Color color;
+
+  void draw()
+  {
+    DrawRectangle(pos.x, pos.y, width, height, color);
+  }
+  void update()
+  {
+    if (pos.y < SCREEN_HEIGHT - height)
+      {
+	  pos.y += 5.0f;
+      }
+  }
+};
+
 int main()
 {
+  Bird bird = {(Vector2){200, 375}, 50, 50, MY_DARK_BLUE};
+
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "flappy bird");
 
   SetTargetFPS(60);
@@ -15,6 +38,8 @@ int main()
     {
       BeginDrawing();
       ClearBackground(MY_PINK);
+      bird.update();
+      bird.draw();
       EndDrawing();
     }
   CloseWindow();
